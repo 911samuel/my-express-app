ccc6a33e27472ea4c9e52808f4a4e2e2a1f34602
911samuel <abayizeraeaz@gmail.com>
2024-02-27 07:28:59 +0200

    update

--- scr/server.ts
+++ scr/server.ts
@@ -8,13 +8,13 @@ import BrandRoute from "./routes/brand";
 import UserRoute from "./routes/auth";
 import commentRoute from "./routes/commentsRoutes";
 
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 import morgan from "morgan";
 
 mongoose
-.connect("mongodb://localhost:27017/test", { bufferCommands: false })
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("MongoDB connection error:", err));
+  .connect("mongodb://localhost:27017/mydatabase")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 
