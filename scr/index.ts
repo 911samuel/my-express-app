0c92c68dd9c6a9c8ad9a84f901eef0ac2ad4e6ad
911samuel <abayizeraeaz@gmail.com>
2024-03-03 00:14:29 +0200

    debugging error connection

--- scr/index.ts
+++ scr/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import path from "path";
 import swaggerUi from "swagger-ui-express";
-import swaggerOutPut from './documentation/swagger_output.json'
+import swaggerOutPut from "./documentation/swagger_output.json";
 
-require('dotenv').config();
+require("dotenv").config();
 
 import blog from "./routes/blogs";
 import user from "./routes/users";
@@ -12,54 +12,55 @@ import comment from "./routes/comments";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
-const connectMongodb = () => {
-  const db = process.env.MYBRAND_MONGODB_URI;
-  if (!db) {
-    console.log("Can not read mongoo string");
-    return;
-  } else {
-    mongoose
-      .connect(db)
-      .then(() => {
-        console.log("dataBase successfully connected");
-      })
-      .catch((err: any) => {
-        console.log("dataBase failed to connect:", err);
-      });
-  }
+const db = process.env.MYBRAND_MONGODB_URI;
+if (!db) {
+  console.log("Can not read mongoo string");
+} else {
+  mongoose
+    .connect(db)
+    .then(() => {
+      console.log("dataBase successfully connected");
+    })
+    .catch((err: any) => {
+      console.log("dataBase failed to connect:", err);
+    });
 }
-
-connectMongodb();
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/users", user
- /* 
+app.use(
+  "/users",
+  user
+  /* 
 #swagger.tags = ['USER']
 */
 );
-app.use("/blogs", blog
-/* 
+app.use(
+  "/blogs",
+  blog
+  /* 
 #swagger.tags = ['BLOG']
 */
 );
-app.use("/comments", comment
-/* 
+app.use(
+  "/comments",
+  comment
+  /* 
 #swagger.tags = ['COMMENT']
 */
 );
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutPut));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutPut));
 
-app.use('/', (req, res) => {
-  res.send('welcome to my brand');
+app.use("/", (req, res) => {
+  res.send("welcome to my brand");
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
