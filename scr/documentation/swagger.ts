3f7ef35ef65a12dcbbf4013412eb4ae2e6ce1c4a
911samuel <abayizeraeaz@gmail.com>
2024-03-04 00:35:47 +0200

    Update swagger.ts file with OpenAPI 3.0.0 specification and add bearer authentication scheme.

--- scr/documentation/swagger.ts
+++ scr/documentation/swagger.ts
@@ -1,22 +1,34 @@
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 const apiDoc = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'MY BRAND API DOCUMENTATION',
-    description: 'Documentation for the Express API endpoints',
-    version: '1.0.0',
+    title: "MY BRAND API DOCUMENTATION",
+    description: "Documentation for the Express API endpoints",
+    version: "1.0.0",
   },
   servers: [
     {
-      url: 'http://localhost:3000',  
-      description: 'Local server',
+      url: "http://localhost:3000",
+      description: "Local server",
     },
   ],
-  paths: {},  
+  paths: {},
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+  },
 };
 
-const outputFilePath = './swagger_output.json';
-const endpointsFilePaths = ['./scr/index.ts']; 
+const outputFilePath = "./swagger_output.json";
+const endpointsFilePaths = ["../index.ts"]; 
 
-swaggerAutogen({ openapi: '3.0.0' })(outputFilePath, endpointsFilePaths, apiDoc);
+swaggerAutogen({ openapi: "3.0.0" })(
+  outputFilePath,
+  endpointsFilePaths,
+  apiDoc
+);
